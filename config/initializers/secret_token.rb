2cfa1b1c9008e631bf28476da5f93334f9c4e526
Abby Howell <abby.g.howell@gmail.com>
2013-12-02 16:54:53 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CatsCuties::Application.config.secret_key_base = '54b0a98f8685402e02e11005b6f312338e16511508321a6b5256c1c268fd929ca78f2243d00374173719082bf11a7d066201a8e39390b8993c8511632d77199e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CatsCuties::Application.config.session_store :cookie_store, key: '_cats_cuties_session'
