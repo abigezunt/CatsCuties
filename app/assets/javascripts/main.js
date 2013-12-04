243c7dcea5d2925d4ed8c24b39dde7c7c67045fb
Abby Howell <abby.g.howell@gmail.com>
2013-12-04 13:40:04 -0500

    precompiling

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,35 +1,34 @@
-/*globals $*/
-'use strict';
+// /*globals $*/
+// 'use strict';
 
-var CatsCuties = {
+// var CatsCuties = {
 
-  updateLineItem: function(data) {
-    $('#line_item_total_price').innerHTML = data;
-  },
+//   updateLineItem: function(data) {
+//     $('#line_item_total_price').innerHTML = data;
+//   },
 
-  callAjax: function(id, quantity) {
-    $.ajax({
-      url: '/line_items/' + id + '.json',
-      type: 'post',
-      dataType: 'json',
-      success: function(data) {
-        CatsCuties.updateLineItem(data);
-      }
-    });
-  },
+//   callAjax: function(id, quantity) {
+//     $.ajax({
+//       url: '/line_items/' + id + '.json',
+//       type: 'post',
+//       dataType: 'json',
+//       success: function(data) {
+//         CatsCuties.updateLineItem(data);
+//       }
+//     });
+//   },
 
-  setChangeEvent: function() {
-    $( '#exampleInputEmail1' ).change(function() {
-      var quantity = $('#exampleInputEmail1').val(),
-        debugger;
-        id = $('#exampleInputEmail1').attr('data_id');
-      CatsCuties.callAjax(id, quantity);
-    });
-  }
+//   setChangeEvent: function() {
+//     $( '#exampleInputEmail1' ).change(function() {
+//       var quantity = $('#exampleInputEmail1').val(),
+//         id = $('#exampleInputEmail1').attr('data_id');
+//       CatsCuties.callAjax(id, quantity);
+//     });
+//   }
 
-};
+// };
 
-$( document ).ready(function() {
+// $( document ).ready(function() {
 
-  CatsCuties.setChangeEvent();
-});
+//   CatsCuties.setChangeEvent();
+// });
